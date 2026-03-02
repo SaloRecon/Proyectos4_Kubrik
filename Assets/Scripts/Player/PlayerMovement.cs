39f0556fea0c0c0bad72b465235dbd903aad2dc4
SaloRecon <salomonfayads@gmail.com>
2026-03-02 20:15:14 +0100

    UpdateInputMapping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,17 +5,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerInput playerInput;
-    private CharacterController playerController;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
-        playerController = GetComponent<CharacterController>();
     }
     
     private void OnEnable()
     {
-            playerInput.actions["Movement"].started += Movement;
+        playerInput.actions["Movement"].started += Movement;
     }
     
     private void OnDisable()
