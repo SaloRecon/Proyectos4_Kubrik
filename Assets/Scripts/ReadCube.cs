7566108cea8a598ac9c3b3ab1f0957219f2f44d7
tomasspano <tomispano05@gmail.com>
2026-03-03 20:30:03 +0100

    mas cambios en el cubo

---
+++ Assets/Scripts/ReadCube.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ReadCube : MonoBehaviour
+{
+    public Transform tUp;
+    public Transform tDown;
+    public Transform tFront;
+    public Transform tLeft;
+    public Transform tRight;
+    public Transform tBack;
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SelectFace.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SelectFace : MonoBehaviour
+{
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
