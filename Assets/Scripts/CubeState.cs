f54d43f64f9e9ae7589f4652e570e12b6ad39e6e
tomasspano <tomispano05@gmail.com>
2026-03-10 18:48:05 +0100

    avance scripts cubo

--- Assets/Scripts/CubeState.cs
+++ Assets/Scripts/CubeState.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 public class CubeState : MonoBehaviour
 {
     //lados
-    
+   
     public List<GameObject> front = new List<GameObject>();
     public List<GameObject> back = new List<GameObject>();
     public List<GameObject> left = new List<GameObject>();
     public List<GameObject> right = new List<GameObject>();
     public List<GameObject> up = new List<GameObject>();
     public List<GameObject> down = new List<GameObject>();
-
-    private int layerMask = 1 << 8; //mascara de capa solamente para las caras
-    private CubeState cubeState;
     
     
     void Start()
     {
-        cubeState = GetComponent<CubeState>();
+       
     }
     
     void Update()
     {
-        List<GameObject> facesHit = new List<GameObject>();
-        Vector3 ray = tFront.transform.position;
-        
+       
+
     }
 }
--- Assets/Scripts/ReadCube.cs
+++ Assets/Scripts/ReadCube.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ReadCube : MonoBehaviour
@@ -9,16 +10,57 @@ public class ReadCube : MonoBehaviour
     public Transform tRight;
     public Transform tBack;
     
-    
+    private int layerMask = 1 << 8; //mascara de capa solamente para las caras
+    private CubeState cubeState;
     
     void Start()
     {
+        cubeState =  GetComponent<CubeState>();
         
+        List<GameObject> facesHit = new List<GameObject>();
+        Vector3 ray = tFront.transform.position;
+        RaycastHit hit;
+        
+        //el raycast toca algún objeto en la layermask?
+        if (Physics.Raycast(ray, tFront.right, out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(ray, tFront.right * hit.distance, Color.red);
+            facesHit.Add(hit.collider.gameObject);
+        }
+        else
+        {
+            Debug.DrawRay(ray, tFront.right * 1000, Color.green);
+        }
+        
+        cubeState.front = facesHit;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+       
     }
+
+   /* List<GameObject> BuildRays(Transform rayTransform, Vector3 rayDirection)
+    {
+        //la rayCount se usa para nombrar los raycasts para asegurar el orden
+        int rayCount = 0;
+        List<GameObject> rays = new List<GameObject>();
+        // esto se ve así:
+        //|0||1||2|
+        //|3||4||4|
+        //|6||7||8|
+
+        for (int y = 1; y > -2; y--)
+        {
+            for (int x = -1; x < 2; x++)
+            {
+                Vector3 startPos = new Vector3(rayTransform.localPosition.x + x,
+                    rayTransform.localPosition.y + y,
+                    rayTransform.localPosition.z);
+               // GameObject rayStart = Instantiate(emptyGO, startPos, Quaternion.identity, rayTransform)
+            }
+        }
+        
+    }*/
+    
 }
