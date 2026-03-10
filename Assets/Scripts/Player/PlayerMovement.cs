ea275528b1affda92a762da298bb8bf5aaf14fb1
SaloRecon <salomonfayads@gmail.com>
2026-03-10 18:00:15 +0100

    PlayerMovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,30 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Variables")]
     [SerializeField] private float movementSpeed;
-    [SerializeField] private float distance;
     
+    
+    [SerializeField] private float sampleDistance;
+    
+    private NavMeshAgent player;
     private PlayerInput playerInput;
-    
+    private Animator playerAnim;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Vector2 mousePosition;
     
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        player = GetComponent<NavMeshAgent>();
+        playerAnim = GetComponent<Animator>();
+
+        player.speed = movementSpeed;
     }
     
     private void OnEnable()
@@ -31,37 +42,25 @@ public class PlayerMovement : MonoBehaviour
     
     private void Movement(InputAction.CallbackContext obj)
     {
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, groundLayer))
+        {
+            if(NavMesh.SamplePosition(hit.point, out NavMeshHit navHit, sampleDistance, NavMesh.AllAreas))
+            {
+                player.SetDestination(navHit.position);
+            }
+            else
+            {
+                Debug.Log("No valid NavMesh position found near the clicked point.");
+            }
+        }
+        
         
     }
         
     private void MousePos(InputAction.CallbackContext obj)
     {
-        Vector2 mouseScreenPosition = obj.ReadValue<Vector2>();
-        
-        // Debug: Log mouse screen position
-        Debug.Log($"<color=yellow>[MOUSE] Screen Position: {mouseScreenPosition}</color>");
-        
-        // Create a ray from camera through mouse position
-        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
-        
-        Debug.Log($"<color=cyan>[RAY] Origin: {ray.origin}, Direction: {ray.direction}</color>");
-        
-        // Raycast to get world position
-        if (Physics.Raycast(ray, out RaycastHit hit, distance))
-        {
-            Vector3 worldPosition = hit.point;
-            
-            Debug.Log($"<color=green>[HIT] World Position: {worldPosition}</color>");
-            Debug.Log($"<color=green>[HIT] Hit Object: {hit.collider.gameObject.name}</color>");
-            Debug.Log($"<color=green>[HIT] Distance from camera: {hit.distance}</color>");
-        }
-        else
-        {
-            // If no hit, use a default distance from camera
-            Vector3 worldPosition = ray.origin + ray.direction * distance;
-            
-            Debug.Log($"<color=orange>[NO HIT] Using default world position at distance {distance}: {worldPosition}</color>");
-        }
+        mousePosition = obj.ReadValue<Vector2>();
     }
     
 }
