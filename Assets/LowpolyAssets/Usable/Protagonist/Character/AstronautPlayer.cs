c405cb12d74ca7f993aed3017a6520a81131fc18
SaloRecon <salomonfayads@gmail.com>
2026-04-14 18:32:21 +0200

    LevelSelector script + scene

--- Assets/LowpolyAssets/Usable/Protagonist/Character/AstronautPlayer.cs
+++ Assets/LowpolyAssets/Usable/Protagonist/Character/AstronautPlayer.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using System.Numerics;
+using UnityEngine.Rendering.Universal.Internal;
+using Vector3 = UnityEngine.Vector3;
 
 namespace AstronautPlayer
 {
@@ -11,7 +14,7 @@ namespace AstronautPlayer
 
 		public float speed = 600.0f;
 		public float turnSpeed = 400.0f;
-		private Vector3 moveDirection = Vector3.zero;
+		private Vector3 moveDirection;
 		public float gravity = 20.0f;
 
 		void Start () {
@@ -22,17 +25,18 @@ namespace AstronautPlayer
 		void Update (){
 			if (Input.GetKey ("w")) {
 				anim.SetInteger ("AnimationPar", 1);
+				controller.Move(moveDirection * Time.deltaTime);
+				
 			}  else {
 				anim.SetInteger ("AnimationPar", 0);
 			}
 
 			if(controller.isGrounded){
-				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+				moveDirection = Vector3.forward * Input.GetAxis("Vertical") * speed;
 			}
 
 			float turn = Input.GetAxis("Horizontal");
 			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
 		}
 	}
