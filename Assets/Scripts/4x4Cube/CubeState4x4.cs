c568da0891f0f7b623112941ba31cd473f1e492e
SaloRecon <salomonfayads@gmail.com>
2026-04-20 19:24:41 +0200

    Revert "continuacion 2"
    This reverts commit 9dd7e106a95c08290349e1ae29c4ac85cecd0ebc.

--- Assets/Scripts/4x4Cube/CubeState4x4.cs
+++ Assets/Scripts/4x4Cube/CubeState4x4.cs
@@ -1,37 +1,16 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeState4x4 : MonoBehaviour
 {
-    public List<GameObject> front, back, left, right, up, down;
-    public GameObject[] pivots; 
-
-    public void PickUp(List<GameObject> cubeSide) 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        GameObject currentPivot = GetPivot(cubeSide);
-        foreach (GameObject face in cubeSide) 
-        {
-            //no hay pieza central, se emparenta al pivote
-            face.transform.parent.transform.parent = currentPivot.transform;
-        }
-        currentPivot.GetComponent<PivotRotation4x4>().Rotate(cubeSide);
+        
     }
 
-    public void PutDown(List<GameObject> littleCubes, Transform pivot) 
+    // Update is called once per frame
+    void Update()
     {
-        foreach (GameObject littleCube in littleCubes) 
-        {
-            littleCube.transform.parent.transform.parent = this.transform; //vuelven al cubo principal
-        }
-    }
-
-    private GameObject GetPivot(List<GameObject> side) 
-    {
-        if (side == up) return pivots[0];
-        if (side == down) return pivots[1];
-        if (side == left) return pivots[2];
-        if (side == right) return pivots[3];
-        if (side == front) return pivots[4];
-        return pivots[5];
+        
     }
 }
