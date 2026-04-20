e73219a3e565782590d5a1049a9d245c8df59eed
SaloRecon <salomonfayads@gmail.com>
2026-04-20 19:22:40 +0200

    Revert "Menu"
    This reverts commit 73a49c75195a050dd3f158cebcf390947a330810.

--- Assets/Scripts/Game Manager/SC_GameManager.cs
+++ Assets/Scripts/Game Manager/SC_GameManager.cs
@@ -1,53 +1,16 @@
-using UnityEngine.SceneManagement;
+using UnityEngine;
 
-//public class GameManager : MonoBehaviour
-//{
-   // public static GameManager Instance;
+public class SC_GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
 
-   // private GameObject player;
-
-   // private void Awake()
-   // {
-        // Singleton
-        //if (Instance == null)
-        //{
-          //  Instance = this;
-          //  DontDestroyOnLoad(gameObject);
-       // }
-        //{
-          //  Destroy(gameObject);
-        //}
-    //}
-
-    //private void Start()
-    //{
-        //player = GameObject.FindGameObjectWithTag("Player");
-    //}
-
-   // public void LoadNextScene(string sceneName)
-    //{
-       // SceneManager.sceneLoaded += OnSceneLoaded;
-       // SceneManager.LoadScene(sceneName);
-    //}
-
-    //private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    //{
-       // SceneManager.sceneLoaded -= OnSceneLoaded;
-
-        // Buscar player si se pierde referencia
-        //if (player == null)
-           // player = GameObject.FindGameObjectWithTag("Player");
-
-        // Buscar StartPlataform
-        //GameObject start = GameObject.Find("StartPlataform");
-
-       /// if (start != null && player != null)
-        //{
-         //   player.transform.position = start.transform.position;
-       // }
-      //  else
-      //  {
-            //Debug.LogWarning("No se encontró StartPlataform o Player");
-        //}
-    //}
-//}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
