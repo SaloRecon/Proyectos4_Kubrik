210105773240f1fb6bc9081be4a411106ca8cc7b
SaloRecon <salomonfayads@gmail.com>
2026-05-24 13:28:43 +0200

    PlayerMovementFinal

--- Assets/ProjectFiles/Scripts/Player/PlayerMovement.cs
+++ Assets/ProjectFiles/Scripts/Player/PlayerMovement.cs
@@ -1,200 +1,208 @@
-using System;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.InputSystem;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
-public class PlayerMovement : MonoBehaviour
+
+namespace ProjectFiles.Scripts.Player
 {
-    [SerializeField] private float movementSmoothFactor;
-    
-    [Header("Movement")] 
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float gravityScale;
-    [SerializeField] private Transform hip;
-    [SerializeField] private Camera mainCamera; 
-    private Vector3 gravityDirection = Vector3.down;
-    
-    [Header("Camera")]
-    [SerializeField] private float rotationSmoothFactor;
-    
-    [Header("Ground Detection")]
-    [SerializeField] private Transform feet;
-    [SerializeField] private float detectionRadius;
-    [SerializeField] private LayerMask whatIsGround;
-
-    private CharacterController controller;
-
-    private bool isGrounded;
-    
-    private Vector2 inputVector; 
-    private Vector3 horizontalMovement; 
-    private Vector3 verticalMovement;
-    private Vector3 totalMovement;
-    
-    private PlayerInput input;
-    
-    [SerializeField] Animator anim;
-    
-    private float rotationVelocity;
-    private float currentSpeed;
-    private float targetSpeed;
-    private float speedVelocity;
-
-    private void Awake()
+    public class PlayerMovement : MonoBehaviour
     {
-        controller = GetComponent<CharacterController>();
-        input = GetComponent<PlayerInput>();
-        
-        if (mainCamera == null)
-            mainCamera = Camera.main;
-    }
-
-    private void OnEnable()
-    {
-        input.actions["Move"].performed += UpdateMovement;
-        input.actions["Move"].canceled += UpdateMovement;
-    }
+        [SerializeField] private float movementSmoothFactor;
     
-    private void OnDisable()
-    {
-        input.actions["Move"].performed -= UpdateMovement;
-        input.actions["Move"].canceled -= UpdateMovement;
-    }
-
-    private void UpdateMovement(InputAction.CallbackContext ctx)
-    {
-        inputVector = ctx.ReadValue<Vector2>();
-    }
+        [Header("Movement")] 
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float gravityScale;
+        [SerializeField] private Transform hip;
+        [SerializeField] private Camera mainCamera; 
+        private Vector3 gravityDirection = Vector3.down;
     
-    void Update()
-    {
-        GroundCheck(); 
-        MoveAndRotate();
-        HipRaycast();
-        ApplyGravity();
-        
-        if (totalMovement.magnitude > 0f)
+        [Header("Camera")]
+        [SerializeField] private float rotationSmoothFactor;
+    
+        [Header("Ground Detection")]
+        [SerializeField] private Transform feet;
+        [SerializeField] private float detectionRadius;
+        [SerializeField] private LayerMask whatIsGround;
+
+        private CharacterController controller;
+
+        private bool isGrounded;
+    
+        private Vector2 inputVector; 
+        private Vector3 horizontalMovement; 
+        private Vector3 verticalMovement;
+        private Vector3 verticalVelocity; 
+        private Vector3 totalMovement;
+    
+        private PlayerInput input;
+    
+        [SerializeField] Animator anim;
+    
+        private float rotationVelocity;
+        private float currentSpeed;
+        private float targetSpeed;
+        private float speedVelocity;
+
+        private void Awake()
         {
-            anim.SetInteger("AnimationPar", 1);
+            controller = GetComponent<CharacterController>();
+            input = GetComponent<PlayerInput>();
+        
+            if (mainCamera == null)
+                mainCamera = Camera.main;
         }
-        
-        Debug.Log(totalMovement.magnitude);
-    }
 
-    private void MoveAndRotate()
-    {
-        targetSpeed = movementSpeed * inputVector.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, movementSmoothFactor);
-        
-        if (inputVector.sqrMagnitude > 0)
+        private void OnEnable()
         {
-            
-            Vector3 cameraForward = mainCamera.transform.forward;
-            Vector3 cameraRight = mainCamera.transform.right;
-            
-            
-            cameraForward = AdaptVectorToGravity(cameraForward);
-            cameraRight = AdaptVectorToGravity(cameraRight);
-            
-            
-            Vector3 moveDirection = (cameraRight * inputVector.x + cameraForward * inputVector.y).normalized;
-            
-            
-            moveDirection = Vector3.ProjectOnPlane(moveDirection, gravityDirection).normalized;
-            
-            if (moveDirection.magnitude > 0.01f)
+            input.actions["Move"].performed += UpdateMovement;
+            input.actions["Move"].canceled += UpdateMovement;
+        }
+    
+        private void OnDisable()
+        {
+            input.actions["Move"].performed -= UpdateMovement;
+            input.actions["Move"].canceled -= UpdateMovement;
+        }
+
+        private void UpdateMovement(InputAction.CallbackContext ctx)
+        {
+            inputVector = ctx.ReadValue<Vector2>();
+        }
+    
+        void Update()
+        {
+            GroundCheck(); 
+            MoveAndRotate();
+            HipRaycast();
+            ApplyGravity();
+        
+            Debug.Log(totalMovement.magnitude);
+        }
+
+        private void MoveAndRotate()
+        {
+            targetSpeed = movementSpeed * inputVector.magnitude;
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, movementSmoothFactor);
+        
+            if (inputVector.sqrMagnitude > 0)
             {
-                horizontalMovement = moveDirection * movementSpeed;
+            
+                Vector3 cameraForward = Vector3.forward;
+                Vector3 cameraRight = mainCamera.transform.right;
+            
+            
+                cameraForward = AdaptVectorToGravity(cameraForward);
+                cameraRight = AdaptVectorToGravity(cameraRight);
+            
+            
+                Vector3 moveDirection = (cameraRight * inputVector.x + cameraForward * inputVector.y).normalized;
+            
+            
+                moveDirection = Vector3.ProjectOnPlane(moveDirection, gravityDirection).normalized;
+            
+                if (moveDirection.magnitude > 0.01f)
+                {
+                    horizontalMovement = moveDirection * movementSpeed;
                 
                 
-                Quaternion targetRotation = Quaternion.LookRotation(moveDirection, -gravityDirection);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime / rotationSmoothFactor);
+                    Quaternion targetRotation = Quaternion.LookRotation(moveDirection, -gravityDirection);
+                    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime / rotationSmoothFactor);
+                }
+                else
+                {
+                    horizontalMovement = Vector3.zero;
+                }
             }
             else
             {
                 horizontalMovement = Vector3.zero;
             }
-        }
-        else
-        {
-            horizontalMovement = Vector3.zero;
-        }
         
-        anim.SetInteger("AnimationPar", inputVector.sqrMagnitude > 0 ? 1 : 0);
         
-        totalMovement = horizontalMovement + verticalMovement;
-        controller.Move(totalMovement * Time.deltaTime);
-    }
-    
-    private Vector3 AdaptVectorToGravity(Vector3 vector)
-    {
-        return Vector3.ProjectOnPlane(vector, gravityDirection).normalized;
-    }
-    
-    private void ApplyGravity()
-    {
-        verticalMovement = gravityDirection * Time.deltaTime * gravityScale;
-    }
-
-    private void GroundCheck()
-    {
-        if (Physics.Raycast(feet.position, gravityDirection, detectionRadius, whatIsGround))
-        {
-            isGrounded = true;
+            anim.SetInteger("AnimationPar", inputVector.sqrMagnitude > 0 ? 1 : 0);
+        
+            totalMovement = horizontalMovement + verticalMovement;
+            controller.Move(totalMovement * Time.deltaTime);
         }
-        else
+    
+        private Vector3 AdaptVectorToGravity(Vector3 vector)
         {
-            isGrounded = false;
+            return Vector3.ProjectOnPlane(vector, gravityDirection).normalized;
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (feet != null)
+    
+    
+        private void ApplyGravity()
         {
+            if (isGrounded)
+            {
+                verticalVelocity = gravityDirection.normalized * 0.1f; 
+            }
+            else
+            {
+                verticalVelocity += gravityDirection.normalized * (gravityScale * Time.deltaTime);
+            }
+        
+            verticalMovement = verticalVelocity;
+        }
+
+        private void GroundCheck()
+        {
+            if (Physics.Raycast(feet.position, gravityDirection, detectionRadius, whatIsGround))
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+        
             Gizmos.DrawSphere(feet.position, detectionRadius);
-        }
         
-        if (hip != null)
-        {
             Gizmos.DrawRay(hip.position, Vector3.right);
-            Gizmos.DrawRay(hip.position, Vector3.left);
+            Gizmos.DrawRay(hip.position, Vector3.left); 
             Gizmos.DrawRay(hip.position, Vector3.down);
             Gizmos.DrawRay(hip.position, Vector3.forward);
             Gizmos.DrawRay(hip.position, -Vector3.forward);
+        
         }
-    }
     
-    private void HipRaycast()
-    {
-        Physics.Raycast(hip.position, Vector3.right, out RaycastHit rightRay, 5f);
-        if(rightRay.collider != null)
+        private void HipRaycast()
         {
-            gravityDirection = Vector3.right;
-        }
+            Physics.Raycast(hip.position, Vector3.right, out RaycastHit rightRay, 5f);
+            if(rightRay.collider != null)
+            {
+                gravityDirection = Vector3.right;
+            }
         
-        Physics.Raycast(hip.position, Vector3.left, out RaycastHit leftRay, 5f);
-        if (leftRay.collider != null)
-        {
-            gravityDirection = Vector3.left;
-        }
+            Physics.Raycast(hip.position, Vector3.left, out RaycastHit leftRay, 5f);
+            if (leftRay.collider != null)
+            {
+                gravityDirection = Vector3.left;
+            }
         
-        Physics.Raycast(hip.position, Vector3.forward, out RaycastHit forwardRay, 5f);
-        if(forwardRay.collider != null)
-        {
-            gravityDirection = Vector3.forward;
-        }
+            Physics.Raycast(hip.position, Vector3.forward, out RaycastHit forwardRay, 5f);
+            if(forwardRay.collider != null)
+            {
+                gravityDirection = Vector3.forward;
+            }
         
-        Physics.Raycast(hip.position, -Vector3.forward, out RaycastHit backwardsRay, 5f);
-        if(backwardsRay.collider != null)
-        {
-            gravityDirection = -Vector3.forward;
-        }
+            Physics.Raycast(hip.position, -Vector3.forward, out RaycastHit backwardsRay, 5f);
+            if(backwardsRay.collider != null)
+            {
+                gravityDirection = -Vector3.forward;
+            }
         
-        Physics.Raycast(hip.position, Vector3.down, out RaycastHit downRay, 5f);
-        if(downRay.collider != null)
-        {
-            gravityDirection = Vector3.down;
+            Physics.Raycast(hip.position, Vector3.down, out RaycastHit downRay, 5f);
+            if(downRay.collider != null)
+            {
+                gravityDirection = Vector3.down;
+            }
         }
     }
 }
