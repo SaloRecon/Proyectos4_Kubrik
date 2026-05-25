2e269e51f7da2d5d7c0d59f69f678df66eff8884
tomasspano <tomispano05@gmail.com>
2026-05-25 12:12:16 +0200

    Tutorial Screen
    solo falta reemplazar la imagen con la nueva actualizada

---
+++ Assets/ProjectFiles/Scripts/Game Manager/TutorialScreen.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TutorialScreen : MonoBehaviour
+{
+    [SerializeField] private Image tutorialScreen;
+    void Start()
+    {
+        tutorialScreen.gameObject.SetActive(true); //solo por las dudas, para que antes del nivel tenga tutorial
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) FadeOut();
+    }
+
+    private void FadeOut()
+    {
+        tutorialScreen.DOFade(0, 1).SetEase(Ease.OutQuad)
+            .OnComplete((() => tutorialScreen.gameObject.SetActive(false)));
+    }
+}
